b166d0e8f45d25ec42d412968af0152796e7e71e
Vaiden43 <vaiden43@gmail.com>
2021-04-02 23:04:38 -0700

    Made it prettier
    Made Changes

--- src/views/FAQ.jsx
+++ src/views/FAQ.jsx
@@ -27,7 +27,7 @@ class Events extends React.Component {
           }}
           {...rest}
         />
-        <Parallax small filter image={require("../assets/img/ShopInterior.jpg")} />
+        <Parallax small filter image={require("../assets/img/2.jpg")} />
         <div className={classNames(classes.main, classes.mainRaised)}>
           <div>
           <GridContainer>
